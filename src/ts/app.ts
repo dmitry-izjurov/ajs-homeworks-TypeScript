19d896fc7639f3ab1b226559fd43afc5fdd1a910
Dmitry <dmitry.izjurov@yandex.ru>
2022-05-18 21:33:44 +0300

    Выполнено задание Новые типы

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,6 +1,7 @@
 import Cart from './service/Cart';
 import Book from './domain/Book';
 import MusicAlbum from './domain/MusicAlbum';
+import Movie from './domain/Movie';
 
 const cart = new Cart();
 console.log(cart.items);
@@ -9,3 +10,6 @@ cart.add(new Book(1001, 'War and Piece', 'Leo Tolstoy', 2000, 1225));
 cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
 
 console.log(cart.items);
+
+const a = new Movie(2012, 'США', 'Avengers Assemble!', ['фантастика', 'боевик', 'фентези', 'приключения'], '137 мин. / 02:17');
+console.log(a);
--- src/ts/domain/Movie.ts
+++ src/ts/domain/Movie.ts
@@ -1,3 +1,10 @@
 export default class Movie {
-    
+    constructor(
+        readonly год: number,
+        readonly страна: string,
+        readonly слоган: string,
+        readonly жанр: Array<any>,
+        readonly время: string
+        ) {
+    }
 }
